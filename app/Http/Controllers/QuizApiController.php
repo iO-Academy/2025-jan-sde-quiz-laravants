f04b11ab04a27c23049373b96228dae8c54f3fdf
Joshua Downton-Lewis <joshuadowntonlewis@outlook.com>
2025-03-24 15:13:28 +0000

    Building QuizApiController and Route.

---
+++ app/Http/Controllers/QuizApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class QuizApiController extends Controller
+{
+    public function all(){
+
+        $quizzes = Quiz::all()->makehidden(['updated_at', 'created_at']);
+//      $quizzes = Quiz::paginate(10);
+
+        $outcome = response() -> json([
+            'message' => 'Quizzes retrieved!',
+            'data' => $quizzes,
+        ]);
+
+        if ($outcome == null){
+            return "Failed to retrieve quizzes...";
+        } else {
+            return $outcome;
+        }
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers;
 
+
+Route::get('/quizzes', [Controllers\QuizApiController::class, 'all']);
