38c7bb1df38a8c7ec7b93859dd21cd349d4e6d71
Jack Leslie <jackandrewleslie@gmail.com>
2025-03-25 16:38:01 +0000

    Fixed put method on quiz route

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/quizzes', [QuizApiController::class, 'all']);
 Route::get('/quizzes/{id}', [QuizApiController::class, 'find']);
-Route::put('/quizzes/{id}', [QuizApiController::class, 'update']);
+Route::put('/quizzes/{quiz}', [QuizApiController::class, 'update']);
 Route::post('/quizzes', [QuizApiController::class, 'create']);
 Route::post('/answers', [AnswerApiController::class, 'create']);
