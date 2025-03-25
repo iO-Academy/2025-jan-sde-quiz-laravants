8233a077d7dfad7e5459f80aa0a7ac20659e6b6b
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-03-25 14:50:45 +0000

    Add answer created

---
+++ app/Http/Controllers/AnswerApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateAnswerRequest;
+use App\Models\Answer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AnswerApiController extends Controller
+{
+    public function create(CreateAnswerRequest $request): JsonResponse
+    {
+        $answer = new Answer();
+        $answer->answer = $request->answer;
+        $answer->correct = $request->correct;
+        $answer->question_id = $request->question_id;
+
+        $answer->save();
+
+        if (!$answer->save()) {
+            return response()-json([
+                'message' => 'Answer creation failed',
+                ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Answer created',
+        ], 201);
+    }
+}
---
+++ app/Http/Requests/CreateAnswerRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateAnswerRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'answer' => 'required|string|min:1',
+            'correct' => 'required|boolean',
+            'question_id' => 'required|integer|exists:questions,id'
+        ];
+    }
+}
