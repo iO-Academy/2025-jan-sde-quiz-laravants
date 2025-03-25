fae52cb3b7caabc6e0c31c6ea6f9e9df09f74f77
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-03-25 11:12:11 +0000

    Tests for story3

--- tests/Feature/QuizApiTest.php
+++ tests/Feature/QuizApiTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Answer;
+use App\Models\Question;
 use App\Models\Quiz;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -15,14 +17,21 @@ class QuizApiTest extends TestCase
     use DatabaseMigrations;
     public function testGetSingleQuizSuccess(): void
     {
-       Quiz::factory()->create();
+        Answer::factory()->create();
+
         $response = $this->get('/api/quizzes/1');
 
         $response->assertStatus(200)
            ->assertJson(function (AssertableJson $json){
                $json->hasAll(['message', 'data'])
                    ->has('data', function (AssertableJson $data){
-                       $data->hasAll(['id', 'name', 'description']);
+                       $data->hasAll(['id', 'name', 'description', 'questions'])
+                           ->has('questions', 1, function (AssertableJson $questions){
+                               $questions->hasAll(['id', 'question', 'hint', 'points', 'answers'])
+                                   ->has('answers', 1, function (AssertableJson $answers){
+                                       $answers->hasAll(['id', 'answer', 'feedback', 'correct']);
+                                   });
+                           });
                    });
            });
     }
