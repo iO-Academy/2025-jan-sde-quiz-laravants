92b78750fcf6a5e205314e2acd104b83538054ba
Chris Hubley <chrisjhubley@gmail.com>
2025-03-25 12:36:32 +0000

    Did post request for creating new question, just need to add testing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\QuestionApiController;
+use App\Http\Controllers\QuizApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/quizzes', [Controllers\QuizApiController::class, 'all']);
+Route::get('/quizzes', [QuizApiController::class, 'all']);
 Route::get('/quizzes/{id}', [QuizApiController::class, 'find']);
+Route::post('/questions', [QuestionApiController::class, 'create']);
