39fb42f0034244955cc458df9731d5e86ffa2eb5
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-03-26 12:14:36 +0000

    Partway through story6 - calculated available_points, question_count, correct_count

--- app/Http/Controllers/ScoreApiController.php
+++ app/Http/Controllers/ScoreApiController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quiz;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ScoreApiController extends Controller
 {
-    public function calculate(Request $request): int
+    public function calculate(Request $request):JsonResponse
     {
 //        $request->validate([
 //            'quiz_id' => 'required|integer',
@@ -15,21 +16,53 @@ class ScoreApiController extends Controller
 //            'answers.*.question_id' => 'required|integer'
 //        ]);
 
-        $question_count = 0;
-        $correct_count = 0;
-        $available_points = 0;
-        $points = 0;
+        $score = [
+            'question_count' => 0,
+            'correct_count' => 0,
+            'available_points' => 0,
+            'points' => 0
+        ];
 
-        $id = $request->quiz_id;
+        $id = $request->quiz;
         $answers = $request->answers;
 
         $quiz = Quiz::with('questions.answers')->find($id);
 
-        foreach ($answers as $answer) {
-            $question_count++;
-            $questions = $quiz->questions = $request->questions;
-        } foreach ($questions as $question) {
-            $available_points += $question->points;
-        } return $available_points;
+//        $answer_id = $request->answers->answer;
+
+
+
+        foreach($answers as $answer) {
+            $questions = $quiz->questions->find($answer['question']);
+            $answer_result = $questions->answers->find($answer['answer']);
+            if($answer_result->correct == 1){
+                $score['correct_count']++;
+            }
+        }
+
+
+
+        // Clue
+//        $question = $quiz->questions->find(2);
+//        $question->answers->find(3);
+
+//        foreach($answers as $answer){
+//            if($->correct == 1) {
+//                $score['correct_count']++;
+//            }
+//        }
+
+        //id of the answer
+
+
+        foreach ($quiz->questions as $question) {
+            $score['available_points'] += $question->points;
+            $score['question_count']++;
+
+        } return response()->json([
+            'message' => 'Score calculated',
+            'data' => $score,
+        ]);
+
     }
 }
