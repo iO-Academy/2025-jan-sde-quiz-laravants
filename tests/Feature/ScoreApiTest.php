8b0de9b5d3fdf75b808a7219da1cb4c0bba111b3
Jack Leslie <jackandrewleslie@gmail.com>
2025-03-26 15:02:55 +0000

    Score API test

--- tests/Feature/ScoreApiTest.php
+++ tests/Feature/ScoreApiTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\Answer;
+use App\Models\Question;
 use App\Models\Quiz;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ScoreApiTest extends TestCase
@@ -14,9 +17,43 @@ class ScoreApiTest extends TestCase
 
     public function test_scores_api_success(): void
     {
-        Quiz::factory()->create();
-        $response = $this->get('/');
+        $quiz = Quiz::factory()->create();
 
-        $response->assertStatus(200);
+        $question1 = Question::factory()->create(['quiz_id' => $quiz->id, 'points' => 1]);
+        $q1answer1 = Answer::factory()->create(['question_id' => $question1->id, 'correct' => 1]);
+        $q1answer2 = Answer::factory()->create(['question_id' => $question1->id, 'correct' => 0]);
+
+        $question2 = Question::factory()->create(['quiz_id' => $quiz->id, 'points' => 4]);
+        $q2answer1 = Answer::factory()->create(['question_id' => $question2->id, 'correct' => 1]);
+        $q2answer2 = Answer::factory()->create(['question_id' => $question2->id, 'correct' => 0]);
+
+
+        $response = $this->post('/api/scores', [
+            'quiz' => $quiz->id,
+            'answers' => [
+                [
+                    'question' => (string) $question1->id,
+                    'answer' =>  $q1answer1->id
+                ],
+                [
+                    'question' => (string) $question2->id,
+                    'answer' => $q2answer2->id
+                ]
+            ]
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(function(AssertableJson $json){
+                $json->hasAll(['message', 'data'])
+                    ->has('data', function(AssertableJson $data) {
+                        $data->hasAll(['points', 'available_points', 'question_count', 'correct_count'])
+                            ->where('points', 1)
+                            ->where('available_points', 5)
+                            ->where('question_count', 2)
+                            ->where('correct_count', 1);
+                    });
+            });
+        // Normal JSON assertions
+        // use $json->where to majke sure the numbers are actualyl right
     }
 }
