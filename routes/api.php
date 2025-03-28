0ae47076532c9af5a25c8b4fc68fbc1660f62253
Jack Leslie <jackandrewleslie@gmail.com>
2025-03-28 09:40:53 +0000

    Fixed error messaging on delete routes

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,11 @@ Route::put('/quizzes/{quiz}', [QuizApiController::class, 'update'])->missing(fun
         'message' => 'Quiz not found',
     ], 404);
 });
-
 Route::put('/questions/{question}', [QuestionApiController::class, 'update'])->missing(function () {
     return response()->json([
         'message' => 'Question not found',
     ], 404);
 });
-
 Route::post('/questions', [QuestionApiController::class, 'create']);
 Route::post('/quizzes', [QuizApiController::class, 'create']);
 Route::post('/answers', [AnswerApiController::class, 'create']);
@@ -28,7 +26,14 @@ Route::put('/answers/{answer}', [AnswerApiController::class, 'update'])->missing
         'message' => 'Answer not found',
     ], 404);
 });
-
-Route::delete('/questions/{question}', [QuestionApiController::class, 'delete']);
-Route::delete('/answers/{answer}', [AnswerApiController::class, 'delete']);
+Route::delete('/questions/{question}', [QuestionApiController::class, 'delete'])->missing(function () {
+    return response()->json([
+        'message' => 'Question not found',
+    ], 404);
+});
+Route::delete('/answers/{answer}', [AnswerApiController::class, 'delete'])->missing(function () {
+    return response()->json([
+        'message' => 'Answer not found',
+    ], 404);
+});
 Route::post('/scores', [ScoreApiController::class, 'calculate']);
